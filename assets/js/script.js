db851fd934a510d095517c853d8e1872a3aceece
WayneX500 <waynexiong500@gmail.com>
2023-07-14 03:48:09 -0500

    sets up html js and css

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        //options here
+        licenseKey: 'gplv3-license',
+        navigation: true,
+        navigationTooltips: ['Solana', 'Yua', "Shiba", 'Sen', 'Xarina', 'Pie']
+    });
+
+    //methods
+});
