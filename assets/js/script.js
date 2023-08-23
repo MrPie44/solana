a6a78b6aadb333b9d5ae356c137ae499364b05ae
WayneX500 <waynexiong500@gmail.com>
2023-08-23 01:30:12 -0500

    fixed links

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,19 @@ $(document).ready(function() {
 
     //methods
 });
+  // power glitch
+  PowerGlitch.glitch('.glitch', {
+    hideOverflow: true
+  })
+    // typewriterjs
+    var typewriterElements = document.querySelectorAll('.typewriter-effect')
+    for (var i = 0; i < typewriterElements.length; i++) {
+      var currentEl = typewriterElements[i]
+      var innerText = currentEl.innerText
+      new Typewriter(currentEl, {
+        loop: true,
+      })
+        .typeString(innerText)
+        .pauseFor(1000)
+        .start()
+    }
